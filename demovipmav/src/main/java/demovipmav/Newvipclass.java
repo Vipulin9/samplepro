fd8a403cfb7576b779f1750b76a776a80b06969b
Vipulin9 <inamdarvipul9@gmail.com>
2021-07-08 09:58:40 +0530

    commit

---
+++ demovipmav/src/main/java/demovipmav/Newvipclass.java
@@ -0,0 +1,11 @@
+package demovipmav;
+
+public class Newvipclass {
+
+	public static void main(String[] args) {
+		System.out.println("this is a new class");
+		// TODO Auto-generated method stub
+
+	}
+
+}
